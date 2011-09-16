8705e24b19980a7e3d2c462ca93f579210d478ff
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2011-09-16 14:59:31 +0300

    remove LightOpenID lib
    it should be loaded outside of the bundle

--- Bridge/LightOpenId/LightOpenId.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Fp\OpenIdBundle\Bridge\LightOpenId;
-
-use LightOpenID as BaseLightOpenId;
-require_once __DIR__ . '/../../vendors/LightOpenID/openid.php';
-
-class LightOpenId extends BaseLightOpenId {}
