302f7d54b67fe02395b3a6559da011e71abbbbbb
Arnaud Le Blanc <arnaud.lb@gmail.com>
2011-12-11 22:16:16 +0100

    Use the new way of registering security factories
    (see https://github.com/symfony/symfony/pull/2454 )

--- FpOpenIdBundle.php
+++ FpOpenIdBundle.php
@@ -2,7 +2,16 @@
 namespace Fp\OpenIdBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Fp\OpenIdBundle\DependencyInjection\Security\Factory\OpenIdFactory;
 
 class FpOpenIdBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new OpenIdFactory);
+    }
 }
