ad3c9a65b19f703dd81326642b7a40e22152b75a
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2012-04-04 10:21:58 +0300

    [controller] start login controller.

---
+++ Controller/SecurityController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Fp\OpenIdBundle\Controller;
+
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class SecurityController extends ContainerAware
+{
+    public function loginAction()
+    {
+    }
+}
